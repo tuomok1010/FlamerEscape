3c8b4dcdcadcba576a5ce6b19b0c7229308fc73c
Spazi-dev <Talishmaprkle@gmail.com>
2020-11-06 14:00:39 +0200

    Prettied up Fires
    they maybe work

--- Assets/Scripts/Flammable.cs
+++ Assets/Scripts/Flammable.cs
@@ -8,7 +8,10 @@ public class Flammable : MonoBehaviour
     [SerializeField] bool isBurningInitially;
 
     Light pointLight;
+    Animator lightAnim;
     ParticleSystem flames;
+    ParticleSystem fire;
+    ParticleSystem glow;
     AudioSource burnSound;
     GameObject ashes;
 
@@ -20,6 +23,7 @@ public class Flammable : MonoBehaviour
     {
         // NOTE point light must be the 0th child of the object!
         pointLight = transform.GetChild(0).GetComponent<Light>();
+        lightAnim = transform.GetChild(0).GetComponent<Animator>();   
         if (pointLight)
         {
             if(isBurningInitially)
@@ -42,24 +46,31 @@ public class Flammable : MonoBehaviour
 
         // NOTE particle system must be the 1st child of the object!
         flames = transform.GetChild(1).GetComponent<ParticleSystem>();
+        fire = transform.GetChild(2).GetComponent<ParticleSystem>();
+        glow = transform.GetChild(3).GetComponent<ParticleSystem>();
         if(flames)
         {
             if(isBurningInitially)
             {
                 flames.Play();
+                fire.Play();
+                glow.Play();
             }
             else
             {
                 flames.Stop();
+                fire.Stop();
+                glow.Stop();
             }
         }
+        
         else
         {
-            Debug.Log("Error! Could not find particle system on " + gameObject.name);
+            Debug.Log("Error! Could not find all particle systems on " + gameObject.name);
         }
 
-        // NOTE: Audio source must be the 2nd child of the object!
-        burnSound = transform.GetChild(2).GetComponent<AudioSource>();
+        // NOTE: Audio source must be the 4nd child of the object!
+        burnSound = transform.GetChild(4).GetComponent<AudioSource>();
         if(burnSound)
         {
             if(isBurningInitially)
@@ -78,8 +89,8 @@ public class Flammable : MonoBehaviour
             Debug.Log("Error! Could not find audio source on " + gameObject.name);
         }
 
-        // NOTE: Ashes gameobject must be the 2nd child of the object!
-        ashes = transform.GetChild(3).gameObject;
+        // NOTE: Ashes gameobject must be the 5nd child of the object!
+        ashes = transform.GetChild(5).gameObject;
         if(ashes)
         {
             ashes.SetActive(false);
@@ -119,9 +130,12 @@ public class Flammable : MonoBehaviour
         {
             isBurning = true;
             pointLight.enabled = true;
+            lightAnim.Play("LightIgnition");
             burnSound.enabled = true;
 
             flames.Play();
+            fire.Play();
+            glow.Play();
             burnSound.Play();
             // Debug.Log("Particle collision between " + name + " and " + other.name);
         }
